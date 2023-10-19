47cf562d74b6eeaccfc53d0ba18cb439999041ae
Georgi Ivanov <georgi.ivanov@scalefocus.com>
2023-10-20 01:20:06 +0300

    Add Panics and print and cleanup

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/GeorgiIvanovSF/mars-rover/inputparser"
@@ -16,8 +17,12 @@ func main() {
 
 	inputLines, err := parser.GetFileLines()
 
-	if err != nil || inputLines == nil {
-		fmt.Printf("There was an Error in parsing the instructions file: %s\n", err.Error())
+	if err != nil {
+		panic(err)
+	}
+
+	if inputLines == nil || len(inputLines) < 3 {
+		panic(errors.New("Not enough lines in input file"))
 	}
 
 	gridLine := inputLines[0]
@@ -25,24 +30,35 @@ func main() {
 	var grid navigation.Grid
 	if parser.ValidateGridLine(gridLine) {
 		grid = parser.GetGridDefinition(gridLine)
+	} else {
+		panic(errors.New("Invalid Grid input: " + gridLine))
+	}
+
+	if !grid.IsValid() {
+		panic(errors.New("Grid was built with invalid arguments: " + gridLine))
 	}
 
 	var rovers []rover.Rover
 
-	if grid.IsValid() && len(inputLines) >= 3 {
+	if len(inputLines) >= 3 {
 		for i := 1; i < len(inputLines); i = i + 2 {
 			if parser.ValidatePositionLine(inputLines[i]) && parser.ValidateInstructionsLine(inputLines[i+1]) {
-				rovers = append(rovers, rover.Rover{
-					Position:     parser.GetPositionDefinition(inputLines[i]),
-					Instructions: navigation.BuildInstructions(inputLines[i+1]),
-				})
+				position := parser.GetPositionDefinition(inputLines[i])
+				if position.IsValid() {
+					rovers = append(rovers, rover.Rover{
+						Position:     position,
+						Instructions: navigation.BuildInstructions(inputLines[i+1]),
+					})
+				}
 			}
 		}
 	}
 
 	for _, rover := range rovers {
+		if len(rover.Instructions) < 1 {
+			panic(errors.New("There is a rover without Instructions at position " + rover.Position.String()))
+		}
 		rover.RunInstructions()
-		fmt.Printf("%d %d %s\n", rover.Position.X, rover.Position.Y, string(rover.Position.Direction))
+		fmt.Println(rover.Position.String())
 	}
-
 }
--- navigation/navigation.go
+++ navigation/navigation.go
@@ -1,5 +1,7 @@
 package navigation
 
+import "fmt"
+
 var compass = map[Direction]map[Instruction]Direction{
 	North: {RotateLeft: West,
 		RotateRight: East},
@@ -11,21 +13,11 @@ var compass = map[Direction]map[Instruction]Direction{
 		RotateRight: North},
 }
 
-func NewPosition(x int, y int, d rune) Position {
-	return Position{
-		X:         x,
-		Y:         y,
-		Direction: Direction(d),
-	}
-}
-
 func (p *Position) ChangeDirection(i Instruction) {
-	// fmt.Printf("Change Direction from %s to %s\n", string(p.Direction), string(compass[p.Direction][i]))
 	p.Direction = compass[p.Direction][i]
 }
 
 func (p *Position) ChangeCoordinates() {
-	// fmt.Printf("Move from X: %d, Y: %d to ", p.X, p.Y)
 	switch p.Direction {
 	case North:
 		p.Y++
@@ -36,9 +28,6 @@ func (p *Position) ChangeCoordinates() {
 	case West:
 		p.X--
 	}
-
-	// fmt.Printf("X: %d, Y: %d\n", p.X, p.Y)
-
 }
 
 func BuildInstructions(line string) []Instruction {
@@ -50,4 +39,8 @@ func BuildInstructions(line string) []Instruction {
 		}
 	}
 	return instructions
+}
+
+func (p *Position) String() string {
+	return fmt.Sprintf("%d %d %s", p.X, p.Y, string(p.Direction))
 }
