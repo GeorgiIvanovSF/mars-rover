7c02f0b3d0a8302e0a1b67d65175ed0e3b88b3a1
Georgi Ivanov <georgi.ivanov@scalefocus.com>
2023-10-20 00:22:51 +0300

    Add File Parser

---
+++ inputparser/parser.go
@@ -0,0 +1,52 @@
+package inputparser
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/GeorgiIvanovSF/mars-rover/navigation"
+)
+
+type Parser struct {
+	FilePath string
+}
+
+func (p *Parser) GetFileLines() ([]string, error) {
+	var lines []string
+
+	file, err := os.Open(p.FilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	file.Close()
+
+	return lines, nil
+}
+
+func (p *Parser) GetGridDefinition(line string) navigation.Grid {
+
+	words := strings.Fields(line)
+	width, _ := strconv.Atoi(words[0])
+	height, _ := strconv.Atoi(words[1])
+
+	return navigation.Grid{Width: width, Height: height}
+}
+
+func (p *Parser) GetPositionDefinition(line string) navigation.Position {
+	words := strings.Fields(line)
+	x, _ := strconv.Atoi(words[0])
+	y, _ := strconv.Atoi(words[1])
+	direction := words[2][0]
+
+	return navigation.Position{X: x, Y: y, Direction: navigation.Direction(direction)}
+}
---
+++ inputparser/validation.go
@@ -0,0 +1,40 @@
+package inputparser
+
+import (
+	"regexp"
+	"strings"
+)
+
+func (p Parser) ValidateGridLine(line string) bool {
+	if !regexp.MustCompile(`^[0-9]+ [0-9]+$`).MatchString(line) {
+		return false
+	}
+
+	words := strings.Fields(line)
+	if len(words) != 2 {
+		return false
+	}
+
+	return true
+}
+
+func (p Parser) ValidatePositionLine(line string) bool {
+	if !regexp.MustCompile(`^[0-9]+ [0-9]+ [NESW]$`).MatchString(line) {
+		return false
+	}
+
+	words := strings.Fields(line)
+	if len(words) != 3 {
+		return false
+	}
+
+	return true
+}
+
+func (p Parser) ValidateInstructionsLine(line string) bool {
+	if !regexp.MustCompile(`^[MLR]+$`).MatchString(line) {
+		return false
+	}
+
+	return true
+}
